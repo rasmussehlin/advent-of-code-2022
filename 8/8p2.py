174523920f4c6e533b817321314723a7ab1bb152
Rasmus Sehlin <rasmus@sehl.in>
2022-12-10 23:57:43 +0100

    Remove unnecessary prints

--- 8/8p2.py
+++ 8/8p2.py
@@ -6,12 +6,10 @@ def calculateScoreForView(treeHeight, trees):
         elif tree >= treeHeight:
             visibleTrees += 1
             break
-    if visibleTrees == 0:
-        print('VISIBLE TREES IS 0')
 
     return visibleTrees
 
-def calculateScenicScore(treeRows, rowIndex, colIndex, printing):
+def calculateScenicScore(treeRows, rowIndex, colIndex):
     if colIndex == 0 or colIndex == len(treeRows[0]) - 1 or rowIndex == 0 or rowIndex == len(treeRows) - 1:
         return 0
 
@@ -41,19 +39,6 @@ def calculateScenicScore(treeRows, rowIndex, colIndex, printing):
     scenicScore = scenicScore * calculateScoreForView(treeHeight, topTrees)
     scenicScore = scenicScore * calculateScoreForView(treeHeight, bottomTrees)
 
-    if printing:
-        print('left:', leftTrees)
-        print('right:', rightTrees)
-        print('top:', topTrees)
-        print('bottom:', bottomTrees)
-        print(treeHeight)
-        print('calculateScoreForView(treeHeight, leftTrees)\t', calculateScoreForView(treeHeight, leftTrees))
-        print('calculateScoreForView(treeHeight, rightTrees)\t', calculateScoreForView(treeHeight, rightTrees))
-        print('calculateScoreForView(treeHeight, topTrees)\t', calculateScoreForView(treeHeight, topTrees))
-        print('calculateScoreForView(treeHeight, bottomTrees)\t', calculateScoreForView(treeHeight, bottomTrees))
-        print(scenicScore)
-        input()
-
     return scenicScore
 
 with open('input.txt') as f:
@@ -66,11 +51,9 @@ with open('input.txt') as f:
     highestScore = 0
     for rowIndex in range(len(scenicScoreMatrix)):
         for colIndex in range(len(scenicScoreMatrix[0])):
-            scenicScoreMatrix[rowIndex][colIndex] = calculateScenicScore(treeRows, rowIndex, colIndex, False)
+            scenicScoreMatrix[rowIndex][colIndex] = calculateScenicScore(treeRows, rowIndex, colIndex)
             if scenicScoreMatrix[rowIndex][colIndex] > highestScore:
                 highestScore = scenicScoreMatrix[rowIndex][colIndex]
-                print(rowIndex, colIndex)
-                calculateScenicScore(treeRows, rowIndex, colIndex, True)
         
         f.write('\t'.join(map(str, scenicScoreMatrix[rowIndex])))
         f.write('\n')
